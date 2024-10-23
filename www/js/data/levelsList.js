97d756d438dcebd740d066c2b68c3be858e84b0b
Franvz <franco.gonzalez12345123@gmail.com>
2024-10-22 21:13:22 -0300

    renderLevels

---
+++ www/js/data/levelsList.js
@@ -0,0 +1,57 @@
+export const levelsList = [
+  {
+    id: 1,
+    img: '',
+    pregunta: '¿Pregunta?',
+    respuestas: {
+      'Pregunta 1': false,
+      'Pregunta 2': true,
+      'Pregunta 3': false,
+      'Pregunta 4': false
+    }
+  },
+  {
+    id: 2,
+    img: '',
+    pregunta: '¿Pregunta?',
+    respuestas: {
+      'Pregunta 1': false,
+      'Pregunta 2': true,
+      'Pregunta 3': false,
+      'Pregunta 4': false
+    }
+  },
+  {
+    id: 3,
+    img: '',
+    pregunta: '¿Pregunta?',
+    respuestas: {
+      'Pregunta 1': false,
+      'Pregunta 2': true,
+      'Pregunta 3': false,
+      'Pregunta 4': false
+    }
+  },
+  {
+    id: 4,
+    img: '',
+    pregunta: '¿Pregunta?',
+    respuestas: {
+      'Pregunta 1': false,
+      'Pregunta 2': true,
+      'Pregunta 3': false,
+      'Pregunta 4': false
+    }
+  },
+  {
+    id: 5,
+    img: '',
+    pregunta: '¿Pregunta?',
+    respuestas: {
+      'Pregunta 1': false,
+      'Pregunta 2': true,
+      'Pregunta 3': false,
+      'Pregunta 4': false
+    }
+  }
+]
